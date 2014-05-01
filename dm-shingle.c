e7c6db74d45ebcbecd696a8d853f26edfd29a1dd
Abutalib Aghayev <agayev@gmail.com>
2014-05-01 08:07:00 -0700

    Added destructor.

--- dm-shingle.c
+++ dm-shingle.c
@@ -12,62 +12,84 @@ struct shingle_c {
   int band_size;
   int cache_percent;
 };
-  
-static int shingle_ctr(struct dm_target *ti, unsigned int argc, char **argv)
+
+static int shingle_get_args(struct dm_target *ti, struct shingle_c *sc,
+                            int argc, char **argv)
 {
-  struct shingle_c *sc;
   unsigned long long tmp;
   char dummy;
   
   if (argc != 4) {
     ti->error = "dm-shingle: Invalid argument count.";
-    return -EINVAL;
-  }
-
-  sc = kmalloc(sizeof(*sc), GFP_KERNEL);
-  if (sc == NULL) {
-    ti->error = "dm-shingle: Cannot allocate shingle context.";
-    return -ENOMEM;
+    return -1;
   }
   
   if (sscanf(argv[1], "%llu%c", &tmp, &dummy) != 1 || tmp & 0xfff ||
       (tmp < 512 * 1024 || tmp > 2 * 1024 * 1024)) {
     ti->error = "dm-shingle: Invalid track size.";
-    return -EINVAL;
+    return -1;
   }
   sc->track_size = tmp;
 
   if (sscanf(argv[2], "%llu%c", &tmp, &dummy) != 1 || tmp < 20 || tmp > 200) {
     ti->error = "dm-shingle: Invalid band size.";
-    return -EINVAL;
+    return -1;
   }
   sc->band_size = tmp;
 
   if (sscanf(argv[3], "%llu%c", &tmp, &dummy) != 1 || tmp < 1 || tmp > 20) {
     ti->error = "dm-shingle: Invalid cache percent.";
-    return -EINVAL;
+    return -1;
   }
   sc->cache_percent = tmp;
 
   if (dm_get_device(ti, argv[0], dm_table_get_mode(ti->table), &sc->dev)) {
     ti->error = "dm-shingle: Device lookup failed.";
-    goto bad;
+    return -1;
+  }
+  return 0;
+}
+
+static void shingle_debug_print(struct shingle_c *sc)
+{
+  DMERR("Constructing...");
+  DMERR("device: %s", sc->dev->name);
+  DMERR("track size: %d", sc->track_size);
+  DMERR("band size: %d", sc->band_size);
+  DMERR("cache percent: %d", sc->cache_percent);
+}
+
+static int shingle_ctr(struct dm_target *ti, unsigned int argc, char **argv)
+{
+  struct shingle_c *sc;
+  
+  sc = kmalloc(sizeof(*sc), GFP_KERNEL);
+  if (sc == NULL) {
+    ti->error = "dm-shingle: Cannot allocate shingle context.";
+    return -ENOMEM;
   }
 
-  printk(KERN_CRIT "dm-shingle: device: %s\n", argv[0]);
-  printk(KERN_CRIT "dm-shingle: track size: %d\n", sc->track_size);
-  printk(KERN_CRIT "dm-shingle: band size: %d\n", sc->band_size);
-  printk(KERN_CRIT "dm-shingle: cache percent: %d\n", sc->cache_percent);
+  if (shingle_get_args(ti, sc, argc, argv) < 0) {
+    kfree(sc);
+    return -EINVAL;
+  }
+
+  shingle_debug_print(sc);
   
   ti->num_flush_bios = 1;
   ti->num_discard_bios = 1;
   ti->num_write_same_bios = 1;
   ti->private = sc;
   return 0;
+}
 
-bad:
+static void shingle_dtr(struct dm_target *ti)
+{
+  struct shingle_c *sc = (struct shingle_c *) ti->private;
+
+  DMERR("Destructing...");
+  dm_put_device(ti, sc->dev);
   kfree(sc);
-  return -EINVAL;
 }
 
 static int shingle_map(struct dm_target *ti, struct bio *bio)
@@ -92,9 +114,10 @@ static int shingle_map(struct dm_target *ti, struct bio *bio)
 
 static struct target_type shingle_target = {
   .name   = "shingle",
-  .version = {1, 1, 0},
+  .version = {1, 0, 0},
   .module = THIS_MODULE,
   .ctr    = shingle_ctr,
+  .dtr    = shingle_dtr,
   .map    = shingle_map,
 };
 

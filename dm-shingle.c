2c62ead6d031609dddc871826df7437d2a3e4129
Abutalib Aghayev <agayev@gmail.com>
2014-05-12 15:05:53 -0700

    dm-shingle now has workqueue support, but is not still complete.

--- dm-shingle.c
+++ dm-shingle.c
@@ -1,14 +1,14 @@
-#include <linux/device-mapper.h>
-
-#include <linux/module.h>
-#include <linux/init.h>
 #include <linux/bio.h>
 #include <linux/bitops.h>
+#include <linux/device-mapper.h>
+#include <linux/init.h>
+#include <linux/mempool.h>
+#include <linux/module.h>
 
 #define DM_MSG_PREFIX "shingle"
 
 /* 
- * Sizes of all names ending with _size is in bytes.
+ * Sizes of all names ending with _size or _SIZE is in bytes.
  */
 
 #define TOTAL_SIZE (76LL << 10)
@@ -19,38 +19,6 @@
 
 #define EXT2INT(sector) ((int32_t) (sector / SECTOR_SIZE_RATIO))
 #define INT2EXT(sector) (((sector_t) sector) * SECTOR_SIZE_RATIO)
-
-struct cache_band {
-        int32_t begin_sector;   /* Where the cache band begins. */
-        int32_t current_sector; /* Where the next write will go. */
-
-        /*
-         * There are usually multiple data bands assigned to each band.
-         * |begin_data_band| is the the first data band assigned to this cache
-         * band.  The next is |begin_data_band + sc->num_cache_bands|.  See
-         * below for its use.
-         */
-        int32_t begin_data_band;
-
-        /*
-         * Although multiple data bands are assigned to a cache band, not all of
-         * them may have sectors on it.  |data_band_bitmap| keeps track of data
-         * bands having sectors on this cache band.  This is used during garbage
-         * collection.
-         *
-         * Assume there are 6 data bands and 2 cache bands.  Then data bands 0,
-         * 2, 4 are assigned to cache band 0 and data bands 1, 3, 5 are assigned
-         * to cache band 1 (using modulo arithmetic).  Consider cache band 1.
-         * If it contains sectors only for data band 5, then |data_band_bitmap|
-         * will be "001", if it contains sectors for data bands 1 and 3, then
-         * |data_band_bitmap| will be "110".  That is, a given data band, its
-         * position in the the bitmap is calculated as below, and the bit in
-         * that position is set:
-         *
-         * |position = (data_band - begin_data_band) / sc->num_cache_bands|
-         */
-        unsigned long *data_band_bitmap;
-};
 
 struct shingle_c {
         struct dm_dev *dev;
@@ -90,45 +58,123 @@ struct shingle_c {
         int32_t num_data_bands;
 
         /* Number of data bands associated with each cache band. */
-        int32_t cache_associativity; 
+        int32_t cache_assoc;
+
+        mempool_t *io_pool;   /* For per bio private data. */
+        struct bio_set *bs;   /* For cloned bios. */
+
+        /* All read and write operations will be put into this queue and will
+         * be performed by worker threads. */
+        struct workqueue_struct *queue;
 };
 
-static int shingle_get_args(struct dm_target *ti, struct shingle_c *sc,
-                            int argc, char **argv)
+struct cache_band {
+        int32_t begin_sector;   /* Where the cache band begins. */
+        int32_t current_sector; /* Where the next write will go. */
+
+        /*
+         * There are usually multiple data bands assigned to each band.
+         * |begin_data_band| is the the first data band assigned to this cache
+         * band.  The next is |begin_data_band + sc->num_cache_bands|.  See
+         * below for its use.
+         */
+        int32_t begin_data_band;
+
+        /*
+         * Although multiple data bands are assigned to a cache band, not all of
+         * them may have sectors on it.  |data_band_bitmap| keeps track of data
+         * bands having sectors on this cache band.  This is used during garbage
+         * collection.
+         *
+         * Assume there are 6 data bands and 2 cache bands.  Then data bands 0,
+         * 2, 4 are assigned to cache band 0 and data bands 1, 3, 5 are assigned
+         * to cache band 1 (using modulo arithmetic).  Consider cache band 1.
+         * If it contains sectors only for data band 5, then |data_band_bitmap|
+         * will be "001", if it contains sectors for data bands 1 and 3, then
+         * |data_band_bitmap| will be "110".  That is, a given data band, its
+         * position in the the bitmap is calculated as below, and the bit in
+         * that position is set:
+         *
+         * |position = (data_band - begin_data_band) / sc->num_cache_bands|
+         */
+        unsigned long *data_band_bitmap;
+};
+
+/*
+ * This represents a single I/O operation, either read or write.  The |base_bio|
+ * associated with it may result in multiple bios being generated, all of which
+ * should completed before this one can be considered completed.  Every
+ * generated bio increments |pending| and every completed bio decrements
+ * pending.  When |pending| reaches zero, we deallocate |io| and signal the
+ * completion of |base_bio| by calling |bio_endio| on it.
+ */
+struct io {
+        struct shingle_c *sc;
+        struct bio *base_bio;
+        struct work_struct work;
+        int error;
+        atomic_t pending;
+};
+
+#define MIN_IOS 16
+#define MIN_POOL_PAGES 32
+
+static struct kmem_cache *_io_pool;
+
+static struct io *io_alloc(struct shingle_c *sc, struct bio *bio)
+{
+        struct io *io = mempool_alloc(sc->io_pool, GFP_NOIO);
+        
+        io->sc = sc;
+        io->base_bio = bio;
+        io->error = 0;
+        atomic_set(&io->pending, 1);
+
+        return io;
+}
+
+static void io_release(struct io *io)
+{
+        struct shingle_c *sc = io->sc;
+        struct bio *base_bio = io->base_bio;
+        int error = io->error;
+
+        BUG_ON(!atomic_dec_and_test(&io->pending));
+        mempool_free(io, sc->io_pool);
+        bio_endio(base_bio, error);
+}
+
+static bool shingle_get_args(struct dm_target *ti, struct shingle_c *sc,
+                             int argc, char **argv)
 {
         unsigned long long tmp;
         char dummy;
   
         if (argc != 4) {
                 ti->error = "dm-shingle: Invalid argument count.";
-                return -1;
+                return false;
         }
 
         /* TODO: FIX THOSE BOUNDS */
         if (sscanf(argv[1], "%llu%c", &tmp, &dummy) != 1 || tmp & 0xfff ||
             (tmp < /* 512 */ 4 * 1024 || tmp > 2 * 1024 * 1024)) {
                 ti->error = "dm-shingle: Invalid track size.";
-                return -1;
+                return false;
         }
         sc->track_size = tmp;
 
         if (sscanf(argv[2], "%llu%c", &tmp, &dummy) != 1 || tmp < 1 /* 20 */ || tmp > 200) {
                 ti->error = "dm-shingle: Invalid band size.";
-                return -1;
+                return false;
         }
         sc->band_size_tracks = tmp;
 
         if (sscanf(argv[3], "%llu%c", &tmp, &dummy) != 1 || tmp < 1 || tmp > 50 /* 20 */) {
                 ti->error = "dm-shingle: Invalid cache percent.";
-                return -1;
+                return false;
         }
         sc->cache_percent = tmp;
-
-        if (dm_get_device(ti, argv[0], dm_table_get_mode(ti->table), &sc->dev)) {
-                ti->error = "dm-shingle: Device lookup failed.";
-                return -1;
-        }
-        return 0;
+        return true;
 }
 
 static void shingle_debug_print(struct shingle_c *sc)
@@ -151,26 +197,11 @@ static void reset_cache_band(struct shingle_c *sc, struct cache_band *cb)
 {
         BUG_ON(!sc || !cb);
         cb->current_sector = cb->begin_sector;
-        bitmap_zero(cb->data_band_bitmap, sc->cache_associativity);
+        bitmap_zero(cb->data_band_bitmap, sc->cache_assoc);
 }
 
-static int shingle_ctr(struct dm_target *ti, unsigned int argc, char **argv)
+static void calc_params(struct shingle_c *sc)
 {
-        struct shingle_c *sc;
-        struct cache_band *cb;
-        int32_t i, sector, data_band_bitmap_size, sector_map_size;
-  
-        sc = kmalloc(sizeof(*sc), GFP_KERNEL);
-        if (!sc) {
-                ti->error = "dm-shingle: Cannot allocate shingle context.";
-                return -ENOMEM;
-        }
-
-        if (shingle_get_args(ti, sc, argc, argv) < 0) {
-                kfree(sc);
-                return -EINVAL;
-        }
-
         sc->total_size = TOTAL_SIZE; /* i_size_read(sc->dev->bdev->bd_inode); */
         sc->band_size = sc->band_size_tracks * sc->track_size;
         sc->band_size_sectors = sc->band_size / INT_SECTOR_SIZE;
@@ -183,7 +214,7 @@ static int shingle_ctr(struct dm_target *ti, unsigned int argc, char **argv)
         sc->num_data_bands = (sc->num_bands / sc->num_cache_bands - 1) *
                 sc->num_cache_bands;
   
-        sc->cache_associativity = sc->num_data_bands / sc->num_cache_bands;
+        sc->cache_assoc = sc->num_data_bands / sc->num_cache_bands;
         sc->usable_size = sc->num_data_bands * sc->band_size;
         sc->wasted_size = sc->total_size - sc->cache_size - sc->usable_size;
         
@@ -192,33 +223,105 @@ static int shingle_ctr(struct dm_target *ti, unsigned int argc, char **argv)
         
         sc->num_usable_sectors = sc->usable_size / INT_SECTOR_SIZE;
         BUG_ON(sc->usable_size % INT_SECTOR_SIZE);
+}
 
-        sector_map_size = (sizeof(int32_t) * sc->num_usable_sectors);
+static bool alloc_structs(struct shingle_c *sc)
+{
+        struct cache_band *cb;
+        int32_t i, sector;
+        int32_t sector_map_size = sizeof(int32_t) * sc->num_usable_sectors;
+        int32_t cache_bands_size = sizeof(*cb) * sc->num_cache_bands;
+        int32_t bitmap_size = BITS_TO_LONGS(sc->cache_assoc) * sizeof(long);
+
         sc->sector_map = vmalloc(sector_map_size);
-        BUG_ON(!sc->sector_map);
+        if (!sc->sector_map)
+                return false;
         memset(sc->sector_map, -1, sector_map_size);
 
-        sc->cache_bands = vmalloc(sizeof(*cb) * sc->num_cache_bands);
-        BUG_ON(!sc->cache_bands);
-  
-        data_band_bitmap_size =
-                BITS_TO_LONGS(sc->cache_associativity) * sizeof(long);
+        sc->cache_bands = vmalloc(cache_bands_size);
+        if (!sc->cache_bands)
+                return false;
+
+        /* The first sector of the cache region is |sc->num_usable_sectors|. */
         sector = sc->num_usable_sectors;
         for (i = 0; i < sc->num_cache_bands; ++i) {
+                unsigned long *bitmap = kmalloc(bitmap_size, GFP_KERNEL);
+                if (!bitmap)
+                        return false;
+                sc->cache_bands[i].data_band_bitmap = bitmap;
+                
                 sc->cache_bands[i].begin_sector = sector;
-                sc->cache_bands[i].data_band_bitmap =
-                        kmalloc(data_band_bitmap_size, GFP_KERNEL);
-                BUG_ON(!sc->cache_bands[i].data_band_bitmap);
                 reset_cache_band(sc, &sc->cache_bands[i]);
                 sector += sc->band_size_sectors;
         }
+        return true;
+}
+
+static void shingle_dtr(struct dm_target *ti);
+
+static int shingle_ctr(struct dm_target *ti, unsigned int argc, char **argv)
+{
+        struct shingle_c *sc;
+        int32_t ret;
+
+        DMERR("Constructing...");
+        
+        sc = kmalloc(sizeof(*sc), GFP_KERNEL);
+        if (!sc) {
+                ti->error = "dm-shingle: Cannot allocate shingle context.";
+                return -ENOMEM;
+        }
+        ti->private = sc;
+
+        if (!shingle_get_args(ti, sc, argc, argv)) {
+                kfree(sc);
+                return -EINVAL;
+        }
+
+        calc_params(sc);
+        
+        ret = -ENOMEM;
+        if (!alloc_structs(sc)) {
+                ti->error = "Cannot allocate data structures.";
+                goto bad;
+        }
+
+        sc->io_pool = mempool_create_slab_pool(MIN_IOS, _io_pool);
+        if (!sc->io_pool) {
+                ti->error = "Cannot allocate mempool.";
+                goto bad;
+        }
+        
+        sc->bs = bioset_create(MIN_IOS, 0);
+        if (!sc->bs) {
+                ti->error = "Cannot allocate bioset.";
+                goto bad;
+        }
+
+        sc->queue = alloc_workqueue("shingled",
+                                    WQ_NON_REENTRANT | WQ_MEM_RECLAIM,
+                                    1);
+        if (!sc->queue) {
+                ti->error = "Cannot allocate work queue.";
+                goto bad;
+        }
+
+        ret = -EINVAL;
+        if (dm_get_device(ti, argv[0], dm_table_get_mode(ti->table), &sc->dev)) {
+                ti->error = "dm-shingle: Device lookup failed.";
+                return -1;
+        }
+        
         shingle_debug_print(sc);
   
         ti->num_flush_bios = 1;
         ti->num_discard_bios = 1;
         ti->num_write_same_bios = 1;
-        ti->private = sc;
         return 0;
+
+bad:
+        shingle_dtr(ti);
+        return ret;
 }
 
 static void shingle_dtr(struct dm_target *ti)
@@ -227,11 +330,26 @@ static void shingle_dtr(struct dm_target *ti)
         struct shingle_c *sc = (struct shingle_c *) ti->private;
 
         DMERR("Destructing...");
-        dm_put_device(ti, sc->dev);
+        
+        ti->private = NULL;
+        if (!sc)
+                return;
+
+        if (sc->queue)
+                destroy_workqueue(sc->queue);
+        if (sc->io_pool)
+                mempool_destroy(sc->io_pool);
+        if (sc->dev)
+                dm_put_device(ti, sc->dev);
+        
         for (i = 0; i < sc->num_cache_bands; ++i)
-                kfree(sc->cache_bands[i].data_band_bitmap);
-        vfree(sc->sector_map);
-        vfree(sc->cache_bands);
+                if (sc->cache_bands[i].data_band_bitmap)
+                        kfree(sc->cache_bands[i].data_band_bitmap);
+
+        if (sc->sector_map)
+                vfree(sc->sector_map);
+        if (sc->cache_bands)
+                vfree(sc->cache_bands);
         kfree(sc);
 }
 
@@ -285,7 +403,7 @@ static void read_modify_write_data_band(struct shingle_c *sc, int32_t db)
 static void garbage_collect(struct shingle_c *sc, struct cache_band *cb)
 {
         int i;
-        for_each_set_bit(i, cb->data_band_bitmap, sc->cache_associativity) {
+        for_each_set_bit(i, cb->data_band_bitmap, sc->cache_assoc) {
                 int32_t db = i * sc->num_cache_bands + cb->begin_data_band;
                 DMERR("GC: data band %d has data in the cache buffer.", db);
                 read_modify_write_data_band(sc, db);
@@ -327,17 +445,76 @@ static int read_bio(struct shingle_c *sc, struct bio *bio)
         return DM_MAPIO_REMAPPED;
 }
 
+static void endio(struct bio *clone, int error)
+{
+        struct io *io = clone->bi_private;
+        
+        if (unlikely(!bio_flagged(clone, BIO_UPTODATE) && !error))
+                io->error = -EIO;
+
+        DMERR("%s %c %lu",
+              (io->error ? "!!" : "OK"),
+              (bio_data_dir(clone) == READ ? 'R' : 'W'),
+              clone->bi_sector);
+
+        bio_put(clone);
+        io_release(io);
+}
+
+static void clone_init(struct io *io, struct bio *clone)
+{
+        struct shingle_c *sc = io->sc;
+
+        clone->bi_private = io;
+        clone->bi_end_io = endio;
+        clone->bi_bdev = sc->dev->bdev;
+}
+
+/*
+ * This function will be executed by the worker threads.
+ */
+static void shingled(struct work_struct *work)
+{
+        struct io *io = container_of(work, struct io, work);
+        struct shingle_c *sc = io->sc;
+        struct bio *base_bio = io->base_bio;
+        struct bio *clone = bio_clone_bioset(base_bio, GFP_NOIO, sc->bs);
+
+        if (!clone) {
+                io->error = -ENOMEM;
+                io_release(io);
+                return;
+        }
+        
+        clone_init(io, clone);
+        generic_make_request(clone);
+}
+
+static void queue_io(struct io *io)
+{
+        struct shingle_c *sc = io->sc;
+        
+        INIT_WORK(&io->work, shingled);
+        queue_work(sc->queue, &io->work);
+        
+}
+
 static int shingle_map(struct dm_target *ti, struct bio *bio)
 {
         unsigned long flags = bio_rw(bio);
         struct shingle_c *sc = ti->private;
-
+        struct io *io = io_alloc(sc, bio);
+        
         BUG_ON(bad_bio(sc, bio));
-        if (flags == READ)
-                return read_bio(sc, bio);
-        if (flags == WRITE)
-                return write_bio(sc, bio);
-        BUG();
+        switch (flags) {
+        case READ:
+        case WRITE:
+                queue_io(io);
+                break;
+        default:
+                BUG();
+        }
+        return DM_MAPIO_SUBMITTED;
 }
 
 static void shingle_status(struct dm_target *ti, status_type_t type,
@@ -372,10 +549,17 @@ static struct target_type shingle_target = {
 
 static int __init dm_shingle_init(void)
 {
-        int r = dm_register_target(&shingle_target);
+        int r;
 
-        if (r < 0)
+        _io_pool = KMEM_CACHE(io, 0);
+        if (!_io_pool)
+                return -ENOMEM;
+        
+        r = dm_register_target(&shingle_target);        
+        if (r < 0) {
                 DMERR("register failed %d", r);
+                kmem_cache_destroy(_io_pool);
+        }
 
         return r;
 }
